e608a90b6f6c49e8457a8e88bb16e1cfdca7597b
rahma-muhammad <68076450+rahma-muhammad@users.noreply.github.com>
2022-09-18 20:57:35 +0200

    Create  1313. Decompress Run-Length Encoded List
    Solved that leetcode problem. Accepted.
    Runtime: 151 ms
    Memory Usage: 14.4 MB
    Link : https://leetcode.com/problems/decompress-run-length-encoded-list/

---
+++ Week 1/Level 1/Decompress Run-Length Encoded List.py
@@ -0,0 +1,11 @@
+class Solution:
+    def decompressRLElist(self, nums):
+        res = []
+        l= len(nums)
+        for i in range(1,l,2):
+            res += [nums[i]] * nums[i-1] 
+
+        return res
+
+t = Solution()
+print(t.decompressRLElist([1,2,3,4]))    # Output: [2,4,4,4]
